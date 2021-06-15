820e0e3476a9e8337f997855d9515d0563be0e31
asraraljuhani <asrarbaljuhani@gmail.com>
2021-06-15 23:19:01 +0300

    edit tables

--- app.py
+++ app.py
@@ -30,29 +30,32 @@ migrate = Migrate(app, db)
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 # show: artist_id, venue_id, start_time
-show = db.Table('show',
-    db.Column('artist_id', db.Integer, db.ForeignKey('artist.id'), primary_key=True),
-    db.Column('venue_id', db.Integer, db.ForeignKey('venue.id'), primary_key=True),
-    db.Column('start_time', db.String, nullable=False)
-)
+
+class Show(db.Model):
+  __tablename__ = 'show'
+  id = db.Column(db.Integer, primary_key=True)
+  artist_id = db.Column('artist_id', db.Integer, db.ForeignKey('artist.id'))
+  venue_id = db.Column('venue_id', db.Integer, db.ForeignKey('venue.id'))
+  start_time = db.Column('start_time', db.DateTime)
+
 
 class Venue(db.Model):
     __tablename__ = 'venue'
 # venue: id, name, city, state, address, phone, genres, image_link,
 # facebook_link, website, seeking_talent, seeking_description, artiest
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120), nullable=True)
-    genres = db.Column(db.String(120),  nullable=False)
-    image_link = db.Column(db.String(500), nullable=True)
-    facebook_link = db.Column(db.String(120), nullable=True)
-    website = db.Column(db.String(120), nullable=True)
-    seeking_talent=db.Column(db.Boolean, nullable=True)
-    seeking_description=db.Column(db.String, nullable=True)
-    artists = db.relationship('Artist', secondary=show, backref=db.backref('venue', lazy=True))
+    name = db.Column(db.String())
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String())
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_talent=db.Column(db.Boolean, default=False)
+    seeking_description=db.Column(db.String())
+    # artists = db.relationship('Artist', secondary=show, backref=db.backref('venue', lazy=True))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate "flask db migrate"
 
@@ -61,16 +64,16 @@ class Artist(db.Model):
 # artist: id, name, city, state, phone, genres, image_link,
 # facebook_link, website, seeking_venue, seeking_description
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120),  nullable=True)
-    genres = db.Column(db.String(120),  nullable=False)
-    image_link = db.Column(db.String(500), nullable=True)
-    facebook_link = db.Column(db.String(120),  nullable=True)
-    website = db.Column(db.String(120),  nullable=True)
-    seeking_venue=db.Column(db.Boolean, nullable=True)
-    seeking_description=db.Column(db.String, nullable=True)
+    name = db.Column(db.String())
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String())
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue=db.Column(db.Boolean, default=False)
+    seeking_description=db.Column(db.String())
     # TODO: implement any missing fields, as a database migration using Flask-Migrate "flask db migrate"
 
 
@@ -102,6 +105,7 @@ def index():
 
 @app.route('/venues')
 def venues():
+  # db.session.query(venue).
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
 #venue: id, name, city, state, address, phone, image_link, facebook_link, artiest
